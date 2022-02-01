0e5b4f7708b560aa1caafd31aca32dadf4456c5f
Paul Przybylowski <paul.daniel.przybylowski@gmail.com>
2022-02-01 15:32:16 -0600

    initial deployment

---
+++ seeds/index.js
@@ -0,0 +1,56 @@
+
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+    useNewUrlParser: true,
+    // useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", () => {
+    console.log("Database connected");
+});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 300; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            
+            //Your User ID
+            author: '61f970d42f3d1c41d0cd627f',
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem sunt mollitia dolore velit. Consequatur quia nostrum recusandae, atque maiores laudantium? Ipsam quos aliquid quaerat a. Voluptatum quod possimus accusantium aliquid.",
+            price,
+            geometry: {
+              type: "Point",
+              coordinates: [
+                cities[random1000].longitude,
+                cities[random1000].latitude,
+              ]
+            },
+            images: [
+                {
+                  url: 'https://res.cloudinary.com/dvfbu2gde/image/upload/v1643736174/YelpCamp/mzlauibqgryiezw0zu3h.jpg',
+                  filename: 'YelpCamp/mzlauibqgryiezw0zu3h'
+                },
+                {
+                  url: 'https://res.cloudinary.com/dvfbu2gde/image/upload/v1643736174/YelpCamp/o5hmxkeqlwqe2msjxfy8.jpg',
+                  filename: 'YelpCamp/o5hmxkeqlwqe2msjxfy8'
+                }
+              ]
+        })
+        await camp.save();
+    }
+}
+
+seedDB();
