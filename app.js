f298ce0d785a6a30827a0932d5a7797b5684561d
Paul Przybylowski <paul.daniel.przybylowski@gmail.com>
2022-02-02 09:10:33 -0600

    update app.js

--- app.js
+++ app.js
@@ -104,8 +104,7 @@ const styleSrcUrls = [
 ];
 const connectSrcUrls = [
     "https://api.mapbox.com/",
-    "https://a.tiles.mapbox.com/",
-    "https://b.tiles.mapbox.com/",
+    "https://*.tiles.mapbox.com/",
     "https://events.mapbox.com/",
 ];
 const fontSrcUrls = [];
